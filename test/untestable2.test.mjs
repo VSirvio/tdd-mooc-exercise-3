c117290684a2f34bd82299362f53984e0b062d2c
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-28 16:24:29 +0300

    untestable2: test #1

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -1,10 +1,14 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { diceHandValue, diceRoll } from "../src/untestable2.mjs";
+import { diceHandValue } from "../src/untestable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(diceHandValue(diceRoll)).to.be.a("number");
+  test("returns the correct value when two fours are rolled", () => {
+    const diceRollResults = [4, 4];
+    const fakeDiceRoll = () => {
+      diceRollResults.push(diceRollResults[0]);
+      return diceRollResults.shift();
+    };
+    expect(diceHandValue(fakeDiceRoll)).to.equal(104);
   });
 });
