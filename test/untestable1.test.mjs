cbceb7e6398f6c84ec70be3dfebbb3dd59429bff
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-28 15:45:41 +0300

    untestable1: test #2

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -6,4 +6,8 @@ describe("Untestable 1: days until Christmas", () => {
   test("returns the correct value when there are 11 days until Christmas", () => {
     expect(daysUntilChristmas(new Date("2025-12-14T12:35:40.771Z"))).to.equal(11);
   });
+
+  test("returns the correct value when there are 0 days until Christmas", () => {
+    expect(daysUntilChristmas(new Date("2000-12-25T21:01:17.813Z"))).to.equal(0);
+  });
 });
