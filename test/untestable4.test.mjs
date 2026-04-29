4e257ee858e2d070016cc31952aed796b9a4d106
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-04-29 16:50:46 +0300

    untestable4: test #1

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -1,7 +1,43 @@
-import { describe, test } from "vitest";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { readFile } from "node:fs/promises";
+import pg from "pg";
+import { PostgresUserDao } from "../src/untestable4.mjs";
 
 describe("Untestable 4: enterprise application", () => {
-  test("todo", async () => {
-    // TODO: write proper tests for both PasswordService and PostgresUserDao
+  let db;
+  let createTablesQuery;
+  let dropTablesQuery;
+  let users;
+  beforeAll(async () => {
+    db = new pg.Pool({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+    });
+
+    createTablesQuery = await readFile("./src/create-tables.sql", { encoding: "utf8" });
+    dropTablesQuery = await readFile("./src/drop-tables.sql", { encoding: "utf8" });
+
+    await db.query(dropTablesQuery);
+  });
+
+  beforeEach(async () => {
+    await db.query(createTablesQuery);
+    users = new PostgresUserDao(db);
+  });
+
+  afterEach(async () => {
+    await db.query(dropTablesQuery);
+  });
+
+  afterAll(() => {
+    db.end();
+  });
+
+  test("returns null when trying to get user that does not exist", async () => {
+    expect(await users.getById(173)).to.be.null;
   });
 });
